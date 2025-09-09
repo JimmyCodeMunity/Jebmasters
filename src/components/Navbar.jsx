7a44786592d3849fd0aad806f0b4d1cbdb7a5c03
Digitalqatalyst <dq.demo.a@digitalqatalyst.com>
2025-09-09 16:20:13 +0300

    navbar updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { navbarlinks } from "../constants";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Menu } from "lucide-react";
 
 const Navbar = ({ bgtransparent }) => {
@@ -10,6 +10,11 @@ const Navbar = ({ bgtransparent }) => {
   const handleMenu = () => {
     setOpen(!open);
   };
+  // et pathname
+  const pathname = useLocation().pathname;
+  console.log(pathname);
+  // set active
+
   return (
     <div
       className={`w-full sm:py-8 py-6 sm:px-16 px-6 ${
@@ -19,16 +24,25 @@ const Navbar = ({ bgtransparent }) => {
       <div className="w-full flex flex-row items-center justify-between">
         <div>
           {/* logo here */}
-          <h1 className="text-orange-500 text-2xl font-semibold">
-            Jebmasters Ltd
-          </h1>
+          <Link to="/">
+            <h1 className="text-orange-500 text-2xl font-semibold">
+              Jebmasters Ltd
+            </h1>
+          </Link>
         </div>
 
         <div className="md:block hidden">
           <ul className="flex flex row items-center space-x-3">
             {navbarlinks?.map((nav) => {
+              const active = pathname == nav?.path;
               return (
-                <li className="text-white hover:text-orange-500">
+                <li
+                  className={`text-white hover:text-orange-500 ${
+                    pathname == nav?.path || pathname.includes(nav?.path)
+                      ? "text-orange-500"
+                      : ""
+                  }`}
+                >
                   <Link to={nav?.path}>{nav?.title}</Link>
                 </li>
               );
@@ -41,12 +55,15 @@ const Navbar = ({ bgtransparent }) => {
           </button>
         </div>
         {open && (
-          <div className="bg-black absolute top-16 right-5 h-32 w-40 rounded-md">
-            <ul className="flex flex-col items-center space-x-3">
+          <div className="bg-black absolute top-16 right-5 min-h-32 py-2 w-40 rounded-md">
+            <ul className="flex flex-col items-center space-y-3">
               {navbarlinks?.map((nav) => {
+                const active = pathname === nav?.path;
                 return (
                   <li
-                    className="text-white hover:text-orange-500"
+                    className={`text-white hover:text-orange-500 ${
+                      active ? "text-orange-500" : ""
+                    }`}
                     onClick={() => handleMenu()}
                   >
                     <Link to={nav?.path}>{nav?.title}</Link>
